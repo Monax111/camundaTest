5b163ee2ee6385bfc82f33e2334b965143372aaf
Monax111 <mar_in@mail.ru>
2019-11-07 02:40:08 +0300

    new wersion

--- src/test/kotlin/local/test/tim/camunda/AppTest.kt
+++ src/test/kotlin/local/test/tim/camunda/AppTest.kt
@@ -1,41 +1,29 @@
 package local.test.tim.camunda
 
-import local.test.tim.camunda.endpoint.ProcessController
-import local.test.tim.camunda.service.CamundaService
-import local.test.tim.camunda.utils.ProcessTestConfig
 import org.camunda.bpm.engine.runtime.ProcessInstance
 import org.camunda.bpm.engine.test.Deployment
-import org.camunda.bpm.engine.test.ProcessEngineRule
-import org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions
-import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareAssertions.assertThat
-import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests
-import org.junit.Rule
-import org.junit.jupiter.api.Assertions.*
+import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat
+import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService
+import org.camunda.bpm.extension.mockito.DelegateExpressions.autoMock
+import org.camunda.bpm.spring.boot.starter.test.helper.AbstractProcessEngineRuleTest
 import org.junit.Test
 
+@Deployment(resources = ["loanApproval.bpmn"])
+class AppTest: AbstractProcessEngineRuleTest() {
 
-class AppTest {
 
-    @get:Rule
-    var processEngineRule = ProcessEngineRule(ProcessTestConfig.processEngine)
-
-    var camundaService = CamundaService(AbstractAssertions
-        .processEngine().runtimeService)
-
-    var processController = ProcessController(camundaService)
 
     @Test
-    @Deployment(resources = ["loanApproval.bpmn"])
     fun checkSome() {
+        autoMock("loanApproval.bpmn")
         //processController.startProcess("loanApproval", "123")
-        val process = createProcess("123")
+        val process = createProcess("123123213")
         assertThat(process).isWaitingAt("WaitMassege")
     }
 }
 
 
-private fun createProcess(appId: String):ProcessInstance = AbstractAssertions
-    .processEngine().runtimeService
+private fun createProcess(appId: String):ProcessInstance = runtimeService()
     .startProcessInstanceByKey(
         "loanApproval", appId
     )
